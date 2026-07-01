7eed2516e521a9f36f912a705685aa81a77e888b
Nurulmaudy <nurulmaudyapriani@gmail.com>
2026-07-01 16:39:42 +0700

    configurasi pertama

---
+++ drizzle.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'drizzle-kit';
+
+export default defineConfig({
+  schema: './src/db/schema.ts',
+  out: './drizzle',
+  dialect: 'mysql',
+  dbCredentials: {
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    database: 'modul_web'
+  }
+});
